3d563e9a2b9f212422c254331c263c385496a5ad
Manmohan Singh <117078139+Manmohansinghyadav@users.noreply.github.com>
2025-04-03 15:56:09 +0530

    port u server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 4000; 
 
 // ✅ Middleware
 app.use(cors());
